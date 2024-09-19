7c59823d4062a2d07d43493ff075e8bb9bafcb69
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-19 18:31:57 -0300

    Usando useState para atualizar valroes

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,16 +1,27 @@
+import {useState} from 'react'
+
+
 function Form(){
 
   function cadastrarUsuario(e) {
     e.preventDefault();
-    console.log("Cadastrou usuario")
+    console.log(`Usuario ${name} foi cadastrado com a senha: ${password}`)
   }
+
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
 
   return (
     <div>
       <h1>Meu cadastro:</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input Type="text" placeholder="Digite seu nome" />
+          <label htmlFor="name">Nome:</label>
+          <input Type="text" id="name" name="name" placeholder="Digite seu nome" onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input Type="password" id="password" name="password"placeholder="Digite seu senha" onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div>
           <input Type="submit" value="Cadastrar" />
