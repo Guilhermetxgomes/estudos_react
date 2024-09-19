343211d633b983da5b41eab83181028a230c3cf1
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-19 16:18:17 -0300

    Aprendendo sobre props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
 
-  const name = 'Guilherme';
-
-  function soma(a,b){
-    return a + b
-  }
-
-  const url = 'http://via.placeholder.com/150'
+  const nome = "Lucas"
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {name}</p>
-      <p>Soma: {soma(1,2)}</p>
-      <img src={url} alt="Minha imagem" />
-      <HelloWorld />
+      <SayMyName nome="Guilherme" />
+      <SayMyName nome="Mateus" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Guilherme" idade="21" profissao="Estudante" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
