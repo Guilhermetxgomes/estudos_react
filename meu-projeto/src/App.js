d825aad498e0b2bb5437f1d1e9ba539c2e45ca42
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-20 14:14:50 -0300

    Aprendendo sobre react-router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,21 +1,32 @@
-import './App.css';
-import {useState} from 'react'
-import SeuNome from './components/SeuNome'
-import Saudacao from './components/Saudacao'
+import {BrowserRouter as Router, Route,Routes, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import Footer from './pages/Footer'
+
 
 function App() {
 
-  const [nome, setNome] = useState()
-
-
-
   return (
-    <div className="App">
-     <h1>State Lift</h1>
-     <SeuNome setNome={setNome} />
-     <Saudacao nome={nome}/>
-    </div>
-  );
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/empresa">Empresa</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+      </ul>
+      <Routes>
+        <Route exact path="/" Component={Home}>
+        </Route>
+        <Route path="/empresa"  Component={Empresa}>
+        </Route>
+        <Route path="/contato"  Component={Contato}>
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
+
+
+  )
 }
 
 export default App;
