063cbbbc47378f1e944603bd2aea8789439c540e
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-09-23 14:55:05 -0300

    Removendo projetos

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -12,7 +12,7 @@ import ProjectCard from '../project/ProjectCard'
 function Projects(){
   const [projects, setProjects] = useState([])
   const [removeLoading, setRemovingLoading] = useState(false)
-
+  const [projectMessage, setProjectMessage] = useState([])
 
   const location = useLocation()
   let message = ''
@@ -37,6 +37,19 @@ function Projects(){
 
   }, [])
 
+  function removeProject(id) {
+    fetch(`http://localhost:2000/projects/${id}`,{
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'
+      },
+
+    }).then(resp => resp.json()).then(data => {
+      setProjects(projects.filter(project => project.id !== id))
+      setProjectMessage("Projeto removido com sucesso")
+    })
+    .catch((err) => console.log(err))
+  }
+
 
   return (
     <div className={styles.project_container}>
@@ -45,6 +58,7 @@ function Projects(){
         <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
       {message && <Message type="success" msg={message} />}
+      {projectMessage && <Message type="success" msg={projectMessage} />}
       <Container customClass="start">
         {projects.length > 0 &&
           projects.map((project) => (
@@ -53,11 +67,12 @@ function Projects(){
             budget={project.budget}
             category={project.category.name}
             key={project.id}
+            handleRemove={removeProject}
             />
           ))
         }
         {!removeLoading && <Loading />}
-        {removeLoading && projects.length === 0 (
+        {removeLoading && projects.length === 0 && (
           <p>Não há projetos cadastrados</p>
         )
 
--- costs/src/components/project/ProjectCard.js
+++ costs/src/components/project/ProjectCard.js
@@ -6,6 +6,11 @@ import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
 
 function ProjectCard({id, name, budget, category, handleRemove}){
 
+  const remove = (e) => {
+    e.preventDefault()
+    handleRemove(id)
+  }
+
   return (
     <div className={styles.project_card}>
       <h4>{name}</h4>
@@ -17,7 +22,7 @@ function ProjectCard({id, name, budget, category, handleRemove}){
       </p>
       <div className={styles.project_card_actions}>
         <Link to="/"><BsPencil />Editar</Link>
-        <button>
+        <button onClick={remove}>
           <BsFillTrashFill /> Excluir
         </button>
       </div>
